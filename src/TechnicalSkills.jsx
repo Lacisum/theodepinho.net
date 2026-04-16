36165d112a7006e1bef78abafb70da833f3042d2
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-04-16 22:44:12 +0200

    Use lighter images

--- src/TechnicalSkills.jsx
+++ src/TechnicalSkills.jsx
@@ -6,17 +6,53 @@ const TechnicalSkills = () => {
     <section id="technicall-skills">
       <h2>Mes compétences techniques</h2>
       <div className="grid tech-skills-grid">
-        <SkillCard imgSrc="logos/java.webp" alt="Java Logo" caption="Java" />
-        <SkillCard imgSrc="logos/python.webp" alt="Python Logo" caption="Python" />
-        <SkillCard imgSrc="logos/html.webp" alt="HTML Logo" caption="HTML" />
-        <SkillCard imgSrc="logos/css.webp" alt="CSS Logo" caption="CSS" />
-        <SkillCard imgSrc="logos/js.webp" alt="JavaScript Logo" caption="JavaScript" />
-        <SkillCard imgSrc="logos/angular.webp" alt="Angular Logo" caption="Angular" />
-        <SkillCard imgSrc="logos/reactjs.webp" alt="ReactJS Logo" caption="ReactJS" />
-        <SkillCard imgSrc="logos/postgresql.webp" alt="PostgreSQL Logo" caption="PostgreSQL" />
-        <SkillCard imgSrc="logos/bash.webp" alt="Bash Logo" caption="Bash" />
-        <SkillCard imgSrc="logos/git.webp" alt="Git Logo" caption="Git" />
-        <SkillCard imgSrc="logos/docker.webp" alt="Docker Logo" caption="Docker" />
+        <SkillCard
+          imgSrc="logos/java-small.webp"
+          alt="Java Logo"
+          caption="Java"
+        />
+        <SkillCard
+          imgSrc="logos/python-small.webp"
+          alt="Python Logo"
+          caption="Python"
+        />
+        <SkillCard
+          imgSrc="logos/html-small.webp"
+          alt="HTML Logo"
+          caption="HTML"
+        />
+        <SkillCard imgSrc="logos/css-small.webp" alt="CSS Logo" caption="CSS" />
+        <SkillCard
+          imgSrc="logos/js-small.webp"
+          alt="JavaScript Logo"
+          caption="JavaScript"
+        />
+        <SkillCard
+          imgSrc="logos/angular-small.webp"
+          alt="Angular Logo"
+          caption="Angular"
+        />
+        <SkillCard
+          imgSrc="logos/reactjs-small.webp"
+          alt="ReactJS Logo"
+          caption="ReactJS"
+        />
+        <SkillCard
+          imgSrc="logos/postgresql-small.webp"
+          alt="PostgreSQL Logo"
+          caption="PostgreSQL"
+        />
+        <SkillCard
+          imgSrc="logos/bash-small.webp"
+          alt="Bash Logo"
+          caption="Bash"
+        />
+        <SkillCard imgSrc="logos/git-small.webp" alt="Git Logo" caption="Git" />
+        <SkillCard
+          imgSrc="logos/docker-small.webp"
+          alt="Docker Logo"
+          caption="Docker"
+        />
       </div>
     </section>
   );
