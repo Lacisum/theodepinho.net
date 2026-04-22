c5672d980f7427224342c4b2c90aca4b7682a5f7
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-04-22 13:29:34 +0200

    Added some GIFs

--- src/App/PageFound/AsideRight.jsx
+++ src/App/PageFound/AsideRight.jsx
@@ -1,5 +1,64 @@
+import "./AsideRight.css";
+
 const AsideRight = () => {
-  return <aside id="aside-right"></aside>;
+  return (
+    <aside id="aside-right">
+      <img
+        src="decoration/html5-valid.gif"
+        alt="W3C logo with the text 'HTML5' and a checkmark that show a site is HTML5-valid."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/css-valid-blue.gif"
+        alt="W3C logo with the text 'CSS' and a checkmark that show a site is CSS-valid."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/css.gif"
+        alt="On the left of the image, there are 9 blocks stacked in the form of a staircase. On the right, there is a text saying 'Made with CSS'."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/css-is-difficult.gif"
+        alt="On the left of the image, there are 9 blocks stacked in the form of a staircase. On the right, there is a red rectangle containing the text 'CSS id difficult'. The text overflows the rectangle."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/www2.gif"
+        alt="The text 'www' in white, in the space. Planet Earth passes behind the text."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/i-like-computer.jpg"
+        alt="The text 'I LIKE COMPUTER' accompanied by an image of a desktop computer."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/nap-time.gif"
+        alt="The text 'I need a nap Now!' accompanied by a square image of a 'Zzz' onomatopoeia."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/wikipedia.gif"
+        alt="A text that says 'Wikipedia' with the Wikipedia logo as the background."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/smileys.gif"
+        alt="Three smiley faces aligned horizontally, changing color regularly."
+        width="88"
+        height="31"
+      />
+    </aside>
+  );
 };
 
 export default AsideRight;
--- src/App/PageFound/main/Home/IdCard.jsx
+++ src/App/PageFound/main/Home/IdCard.jsx
@@ -3,6 +3,11 @@ import "./IdCard.css";
 const IdCard = () => {
   return (
     <section id="id-card">
+      <img
+        src="decoration/welcome.gif"
+        alt="A computer screen that displays 'Welcome to my website', surrounded by cats playing."
+        height="181"
+      />
       <p>
         Bonjour, je suis Théo de Pinho, étudiant en 2e année de master
         d'informatique.
