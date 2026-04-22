8acb615da02b3c6d8b8185f47ba4c4ee8e3736ff
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-04-22 10:45:58 +0200

    ID card: removed my portrait

--- src/App/PageFound/main/CV.jsx
+++ src/App/PageFound/main/CV.jsx
@@ -1,7 +1,7 @@
-import TechnicalSkills from "./CV/TechnicalSkills";
-import Projects from "./CV/Projects";
 import Education from "./CV/Education";
+import Projects from "./CV/Projects";
 import ProfessionalExperiences from "./CV/ProfessionalExperiences";
+import TechnicalSkills from "./CV/TechnicalSkills";
 
 const CV = () => {
   return (
--- src/App/PageFound/main/Home/IdCard.jsx
+++ src/App/PageFound/main/Home/IdCard.jsx
@@ -3,12 +3,6 @@ import "./IdCard.css";
 const IdCard = () => {
   return (
     <section id="id-card">
-      <img
-        src="moi_circle.webp"
-        alt="Portrait photo de Théo de Pinho"
-        width="160"
-        height="160"
-      />
       <p>
         Bonjour, je suis Théo de Pinho, étudiant en 2e année de master
         d'informatique.
