0fdfb0f9bf91a1b772a431c43562ef02fc970c71
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-04-25 22:31:00 +0200

    Added more GIFs & translated their alt text in french

--- src/App/PageFound/AsideRight.jsx
+++ src/App/PageFound/AsideRight.jsx
@@ -5,55 +5,73 @@ const AsideRight = () => {
     <aside id="aside-right">
       <img
         src="decoration/html5-valid.gif"
-        alt="W3C logo with the text 'HTML5' and a checkmark that show a site is HTML5-valid."
+        alt="Le logo du W3C accompanié du texte 'HTML5' et d'une coche indiquant que le site est conforme à la norme HTML5."
         width="88"
         height="31"
       />
       <img
         src="decoration/css-valid-blue.gif"
-        alt="W3C logo with the text 'CSS' and a checkmark that show a site is CSS-valid."
+        alt="Le logo du W3C accompanié du texte 'CSS' et d'une coche indiquant que le site est conforme à la norme CSS."
         width="88"
         height="31"
       />
       <img
-        src="decoration/css.gif"
-        alt="On the left of the image, there are 9 blocks stacked in the form of a staircase. On the right, there is a text saying 'Made with CSS'."
+        src="decoration/best-viewed-with-any-browser.gif"
+        alt="Le texte 'Best viewed with ANY BROWSER' accompagné par une image animée montrant successivement le logo de Netscape, le logo d'Internet Explorer et la planète Terre."
         width="88"
         height="31"
       />
       <img
-        src="decoration/css-is-difficult.gif"
-        alt="On the left of the image, there are 9 blocks stacked in the form of a staircase. On the right, there is a red rectangle containing the text 'CSS id difficult'. The text overflows the rectangle."
+        src="decoration/powered-by-hello-world.gif"
+        alt="Le texte 'Powered by Hello world'. Les mots 'Hello world' sont plus gros, utilisent une police plus fantaisiste et sont sur un fond bleu et rouge. 'Hello' est en bleu et 'world' en orange."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/got-html5.gif"
+        alt="Le logo de HTML5 accompanié du texte 'GOT HTML5?'."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/i-dream-in-html.gif"
+        alt="Le texte 'I DREAM IN HTML' en police monospace. Chaque mot est entouré de chevrons, à la manière des balises HTML."
         width="88"
         height="31"
       />
       <img
         src="decoration/www2.gif"
-        alt="The text 'www' in white, in the space. Planet Earth passes behind the text."
+        alt="Le texte 'www' en gras et en blanc, flottant dans l'espace. La planète Terre passe derrière le texte au cours d'une animation."
+        width="88"
+        height="31"
+      />
+      <img
+        src="decoration/internet-now.gif"
+        alt="Le texte 'Internet Now!' accompagné par une image montrant la moitié du logo de Netscape et la moitié du logo d'Internet Explorer."
         width="88"
         height="31"
       />
       <img
         src="decoration/i-like-computer.jpg"
-        alt="The text 'I LIKE COMPUTER' accompanied by an image of a desktop computer."
+        alt="Une image d'un ordinateur de bureau avec le texte 'I LIKE COMPUTER'."
         width="88"
         height="31"
       />
       <img
-        src="decoration/nap-time.gif"
-        alt="The text 'I need a nap Now!' accompanied by a square image of a 'Zzz' onomatopoeia."
+        src="decoration/piano.gif"
+        alt="Une animation avec un clavier de piano et du texte japonais."
         width="88"
         height="31"
       />
       <img
-        src="decoration/wikipedia.gif"
-        alt="A text that says 'Wikipedia' with the Wikipedia logo as the background."
+        src="decoration/jpmusic.gif"
+        alt="En bas, une portée de musique avec une mélodie dont les notes s'agitent et changent de couleur constamment. En haut, du texte japonais."
         width="88"
         height="31"
       />
       <img
-        src="decoration/smileys.gif"
-        alt="Three smiley faces aligned horizontally, changing color regularly."
+        src="decoration/88by31.gif"
+        alt="Le texte '88by31' en noir."
         width="88"
         height="31"
       />
