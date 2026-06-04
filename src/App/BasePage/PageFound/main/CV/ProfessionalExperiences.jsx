8eaf79aee141f445833f8ccd5df495c4eb082f0c
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-06-05 00:09:01 +0200

    :sparkles: added Solfy professional experience in CV

--- src/App/BasePage/PageFound/main/CV/ProfessionalExperiences.jsx
+++ src/App/BasePage/PageFound/main/CV/ProfessionalExperiences.jsx
@@ -11,6 +11,14 @@ const ProfessionalExperiences = () => {
       <ol id="jobs-list" className="cv-list">
         <li className="job">
           <h4 className="title">
+            Stage - Développement de l'application et du site web de Solfy
+          </h4>
+          <div className={`details ${theme}`}>
+            Mai 2026 - en cours, Solfy
+          </div>
+        </li>
+        <li className="job">
+          <h4 className="title">
             Stage - Développement du site web <code>dezrann.net</code>
           </h4>
           <div className={`details ${theme}`}>
