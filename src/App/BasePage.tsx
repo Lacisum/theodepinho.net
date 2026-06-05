c63f41a75bb0ab9960e30d5ecae39c6161e46554
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-06-05 22:09:09 +0200

    :label: BasePage.tsx: simplify types

--- src/App/BasePage.tsx
+++ src/App/BasePage.tsx
@@ -1,19 +1,27 @@
 import { Outlet } from "react-router-dom";
-import { ChangeEventHandler, useContext } from "react";
+import { ChangeEvent, useContext } from "react";
 
 import "./BasePage.css";
 import { ThemeContext, Theme } from "../Theme";
 
 const BasePage = () => {
     const { theme, setTheme } = useContext(ThemeContext);
-    const handleThemeSwitchButtonChange: ChangeEventHandler<HTMLInputElement, HTMLInputElement> = (event) => {
-        setTheme(event?.currentTarget?.checked ? Theme.DARK : Theme.LIGHT);
+    const handleThemeSwitchButtonChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setTheme(event.currentTarget.checked ? Theme.DARK : Theme.LIGHT);
     }
     return (
         <div id="base-page" className={theme}>
             <div id="theme-switch-container">
-                <label id="theme-switch-button" className={theme} title="Changer le thème">
-                    <input type="checkbox" defaultChecked={theme === Theme.DARK} onChange={handleThemeSwitchButtonChange}/>
+                <label
+                    id="theme-switch-button"
+                    className={theme}
+                    title="Changer le thème"
+                >
+                    <input
+                        type="checkbox"
+                        defaultChecked={theme === Theme.DARK}
+                        onChange={handleThemeSwitchButtonChange}
+                    />
                     {theme === Theme.DARK && '☀️'}
                     {theme === Theme.LIGHT && '🌘'}
                 </label>
