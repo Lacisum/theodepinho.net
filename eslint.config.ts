79821bb3cf079f58d7d8b3af896e31400f07f295
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-06-06 09:48:07 +0200

    :wrench: fix TypeScript configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,3 @@
-import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
@@ -10,18 +8,13 @@ export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        project: ['./tsconfig.json'],
       },
     },
     rules: {
