14abf0729a55a86c684c7fdb9d3f4036c809efea
Lacisum <150604772+Lacisum@users.noreply.github.com>
2026-06-06 16:15:28 +0200

    :wrench: configure ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,80 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import css from '@eslint/css';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
+
+// https://typescript-eslint.io/rules/no-unused-vars/#what-benefits-does-this-rule-have-over-typescript
+const NO_UNUSED_VARS_OPTIONS = {
+  args: 'all',
+  argsIgnorePattern: '^_',
+  caughtErrors: 'all',
+  caughtErrorsIgnorePattern: '^_',
+  destructuredArrayIgnorePattern: '^_',
+  varsIgnorePattern: '^_',
+  ignoreRestSiblings: true,
+};
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+  {
+    files: ['**/*.js'],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
+      // https://github.com/eslint/eslint/blob/main/packages/js/src/configs/eslint-recommended.js
+      js.configs.recommended,
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/eslintrc/recommended.ts
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
+      globals: globals.browser,
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      '@typescript-eslint/no-unused-vars': ['error', NO_UNUSED_VARS_OPTIONS],
+    },
+  },
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/package-lock.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+    rules: {
+      'json/no-empty-keys': 'off',
+    },
+  },
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'],
+    rules: {
+      'css/no-important': 'off',
+      'css/use-baseline': [
+        'error',
+        {
+          // TODO: remove and instead use baseline CSS
+          allowSelectors: ['nesting', 'has'],
+          allowProperties: ['user-select', 'scrollbar-color', 'text-wrap'],
+        },
+      ],
+      'css/no-invalid-properties': ['error', { allowUnknownVariables: true }],
     },
   },
 ]);
